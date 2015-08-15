50507703f950921ac411c6dc2bdcbf0684436040
ashu <ashu@example.com>
2015-08-15 23:59:50 +0530

    created admin panel, set up databases, wrote some view.py files

--- project1/course/admin.py
+++ project1/course/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Courses
+
+admin.site.register(Courses)
+
+from .models import Announcements
+
+admin.site.register(Announcements)
+
+from .models import Comments
+
+admin.site.register(Comments)
+
+from .models import Reply
+
+admin.site.register(Reply)
+
+
+from .models import Token
+
+admin.site.register(Token)
+
+
+from .models import Course_Roll_Matching_Student
+
+admin.site.register(Course_Roll_Matching_Student)
+
+
+from .models import Course_Roll_Matching_TA
+
+admin.site.register(Course_Roll_Matching_TA)
+
--- project1/login/admin.py
+++ project1/login/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student
+
+admin.site.register(Student)
+
+from .models import UserProfile
+
+admin.site.register(UserProfile)
+
+from .models import Role
+
+admin.site.register(Role)
+
+from .models import Departments
+
+admin.site.register(Departments)
+
+from .models import TA
+
+admin.site.register(TA)
+
+from .models import Instructor
+
+admin.site.register(Instructor)
+
